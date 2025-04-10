ec0e2a9811c1cf21d2cd0ceadd0c4677157b40d5
tahomee <thaothanh6g@gmail.com>
2025-04-10 10:48:04 +0700

    setup feature flag

---
+++ FeatureFlag.js
@@ -0,0 +1,12 @@
+// src/featureFlags.js
+
+const featureFlags = {
+  enableThemeToggle: true,
+  enableSearchModal: true,
+  enableHelp: true,
+  enableNotifications: true,
+  showCard13: false,
+  enableNewUI: false,
+};
+
+export default featureFlags;
